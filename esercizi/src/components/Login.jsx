ff3e228d9c7abd5a199c688423972b0318492e06
4mn3s1498 <ele24980@gmail.com>
2026-05-23 20:13:55 +0200

    live

---
+++ esercizi/src/components/Login.jsx
@@ -0,0 +1,42 @@
+import { useState } from 'react';
+
+export function Login() {
+    const [users, setUsers] = useState(JSON.parse(localStorage.getItem("users")) || [])
+    const [user, setUser] = useState({
+        email: '',
+        password: '',
+    })
+    const [error, setError] = useState(null)
+    const [message, setMessage] = useState(null)
+    function handleChange(event) {
+
+        const { name, value } = event.target
+
+        setUser((prev) => ({ ...prev, [name]: value }))
+    }
+    function handleLogin(event) {
+        event.preventDefault()
+        const userExist = users.find((u) => u.email === user.email && u.password === user.password)
+        if (userExist) {
+            setMessage("Credenziali corrette")
+            setError(null)
+            localStorage.setItem("isLoggedIn", true)
+        } else {
+            setMessage(null)
+            setError("Credenziali errate")
+            localStorage.setItem("isLoggedIn", false)
+
+        }
+    }
+    return (
+        <><form onSubmit={handleLogin}>
+            <input type="email" name="email" onChange={handleChange} placeholder="Inserisci la tua email" />
+            <input type="password" name="password" onChange={handleChange} placeholder="Inserisci la tua password" />
+            <button type="submit">Login</button>
+            </form>
+            {error && <p>{error}</p>}
+            { message && <p>{message}</p>}
+        </>
+    )
+
+}
